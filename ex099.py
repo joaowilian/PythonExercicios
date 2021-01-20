eaee5e537b9e4b4d4f8116311c05b3304f6b7aad
joaowp <joaowilian@yahoo.com.br>
2021-01-19 22:37:01 -0300

    Initial commit

---
+++ ex099.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+def maior(*num):
+    cont = 0
+    print('-='*20)
+    print('Analisando os valores passados....')
+    for x in num:
+        print(f'{x}',end=' ',flush=True)
+        cont += 1
+        sleep(0.5)
+    print(f'\nForam informados {len(num)} valores ao todo.')
+    print(f'O maior valor foi {max(num)}')
+
+
+maior(2,5,83,1,8)
+maior(1,2)
+maior(1,5,6,5,5,5,8,4,2,3)
